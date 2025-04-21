d184337e8b56785f4d53a6724595286e3f598565
Andres Felipe Rico <andrespro2004@gmail.com>
2025-04-20 20:51:49 -0500

    Se agrego metodo de calcular total de los gastos fijos

--- back/routes/fixedExpenses.routes.js
+++ back/routes/fixedExpenses.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getFixedExpensesByUser, createFixedExpense  } = require('../controllers/fixedExpenses.controller');
+const { getFixedExpensesByUser, createFixedExpense, getTotalFixedExpensesByUser  } = require('../controllers/fixedExpenses.controller');
 
 // Obtener metas por UID
 router.get('/fixed_expenses/:uid', getFixedExpensesByUser);
 
 // Crear nueva meta
 router.post('/fixed_expenses', createFixedExpense);
+
+// Obtener total de gastos fijos por UID
+router.get('/fixed_expenses/total/:uid', getTotalFixedExpensesByUser);
 
 module.exports = router;
